8395344cd073786f769dfbb85f99bfd62d25f033
vinodk229 <kumarvinod248@gmail.com>
2026-08-19 10:53:46 +0530

    changes file

--- frontend/src/pages/WorkshopsConducted.jsx
+++ frontend/src/pages/WorkshopsConducted.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import assets from '../assets/assets'
-
+import { BsTwitterX } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
 const schedules = [
   {
     sno: 1,
@@ -82,16 +85,48 @@ const schedules = [
 const WorkshopsConducted = () => {
   return (
     <>
-     <div
-        className="w-full min-h-[280px] flex items-center justify-center relative overflow-hidden bg-cover bg-center"
-        style={{ backgroundImage: `url(${assets.htainBanner})` }}
-      >
-        <div className="absolute inset-0 bg-black/50"></div>
-        <h1 className="relative z-10 text-white text-4xl font-bold uppercase">
-        Workshops Conducted
-        </h1>
-      </div>
-    
+     <nav className="breadcrumb-banner relative overflow-hidden">
+        <div className="container mx-auto px-5 lg:px-8 py-8 lg:py-14">
+            <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-8">
+            <div>
+                <h1 className="text-2xl lg:text-4xl font-medium text-white">
+                Workshops  <span className="font-medium">Conducted</span>
+                </h1>
+
+                <div className="flex items-center gap-3 mt-4 text-sm">
+                <a
+                    href="/" className="text-gray-300 font-medium hover:text-blue-300 transition"
+                >
+                    Home
+                </a>
+
+                <span className="text-slate-400">&gt;</span>
+
+                <span className="text-white">
+                   Workshops Conducted
+                </span>
+                </div>
+            </div>
+
+            <div className="flex-grow-0 lg:flex items-center gap-5 bg-violet-500 px-6 py-3 rounded-full shadow-lg hidden">
+                <button className="text-white hover:scale-110 transition">
+                  <BsTwitterX/>
+                </button>
+
+                <button className="text-white hover:scale-110 transition">
+                <FaLinkedinIn/>
+                </button>
+
+                <button className="text-white hover:scale-110 transition">
+                  <FaFacebookF/>
+                </button>
+                <button className="text-white hover:scale-110 transition">
+                  <FaInstagram/>
+                </button>
+            </div>
+        </div>
+        </div>
+      </nav>
    
      <section className="bg-slate-50 py-16">
       <div className="container mx-auto px-4">
